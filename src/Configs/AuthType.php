490e4a6e8d64534016a78968413eb236e57c4adc
songvi <songvi2025@gmail.com>
2017-10-01 13:48:26 +0200

    Add authtype

---
+++ src/Configs/AuthType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AuthStack\Configs;
+
+class AuthType {
+    const LDAP  = "ldap";
+    const SQL   = "sql";
+    const OIDC  = "oidc";
+    const SAML  = "saml";
+    const CAS   = "cas";
+    const OAUTH2 = "oauth2";
+}
