3decb0a7ea3b054f1fe23bf0041eacbe828bb47d
songvi <songvi2025@gmail.com>
2017-10-01 13:22:22 +0200

    Fix exception class

---
+++ src/Exceptions/ConfigNotFoundException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace AuthStack\Exceptions;
+
+class ConfigNotFoundException extends AuthStackException{}
+
+
---
+++ src/Exceptions/IdentityNotFoundException.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace AuthStack\Exceptions;
+
+class IdentityNotFoundException extends AuthStackException{}
+
